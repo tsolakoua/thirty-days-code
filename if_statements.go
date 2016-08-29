8a1ec60731299de837429b68595c311cc584f62e
Anna Tsolakou <tsolakou.a@gmail.com>
2016-08-29 18:42:17 +0200

    Add files via upload

---
+++ if_statements.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Enter your code here. Read input from STDIN. Print output to STDOUT
+
+	var n int
+	fmt.Scan(&n)
+
+	if 1 <= n && n <= 100 {
+
+		if n%2 == 1 {
+			fmt.Printf("Weird")
+
+		} else if n%2 == 0 && 2 <= n && n <= 5 {
+			fmt.Printf("Not Weird")
+		} else if n%2 == 0 && 6 <= n && n <= 20 {
+			fmt.Printf("Weird")
+		} else if n > 20 {
+			fmt.Printf("Not Weird")
+		}
+
+	}
+
+}

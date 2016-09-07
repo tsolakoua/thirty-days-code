5bffc0c9d12cde56e1a07ffe4c533502b6425a61
Anna Tsolakou <tsolakou.a@gmail.com>
2016-09-07 16:19:17 +0200

    Add files via upload
    Day 13

---
+++ AbstractClasses/src/Book.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+abstract class Book {
+
+    String title;
+    String author;
+
+    Book(String t, String a) {
+        title = t;
+        author = a;
+    }
+
+    abstract void display();
+
+}
---
+++ AbstractClasses/src/MyBook.java
@@ -0,0 +1,16 @@
+class MyBook extends Book {
+    
+    int price;
+
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    @Override
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    } 
+}

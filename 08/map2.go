087aae3bec1fcc8a180c08642ceb9627a2d4ade9
Anna Tsolakou <tsolakou.a@gmail.com>
2016-09-09 20:08:57 +0200

    Update map2.go

--- 08/map2.go
+++ 08/map2.go
@@ -10,26 +10,21 @@ func main() {
 	var key string
 	var value uint64
 	fmt.Scan(&bookSize)
-
 	contactBook := make(map[string]uint64)
 
 	for i := 0; i < bookSize; i++ {
-
 		fmt.Scan(&key)
 		fmt.Scan(&value)
 		contactBook[key] = value
 	}
-
-
 	scanner := bufio.NewScanner(os.Stdin)
+	
 	for scanner.Scan() {
-
-	query := scanner.Text()
-
-	if _, found := contactBook[query]; found {
-		fmt.Print(query,"=",contactBook[query], "\n")
-		} else {
-			fmt.Println("Not found")
+		query := scanner.Text()
+		if _, found := contactBook[query]; found {
+			fmt.Print(query,"=",contactBook[query], "\n")
+			} else {
+			    fmt.Println("Not found")
 		}
 	}
 	
